cebcb6dde6cf014d4c964beee006fffed4402e59
Wei Lee <weilee.rx@gmail.com>
2023-02-12 12:31:06 +0800

    dependency: use pelican.plugins.* to replace old style plugins

--- pelicanconf.py
+++ pelicanconf.py
@@ -91,10 +91,10 @@ MARKDOWN = {
 PLUGIN_PATHS = ["pelican-plugins"]
 PLUGINS = [
     "another_read_more_link",
-    "share_post",
     "i18n_subsites",
     "post_stats",
-    "render_math",
+    "pelican.plugins.render_math",
+    "pelican.plugins.share_post",
     "pelican.plugins.seo",
 ]
 ANOTHER_READ_MORE_LINK = ""
